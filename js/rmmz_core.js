7dff2a8a7c64e21062c8f77e660fd7aa8b9ec2b6
munokura <munokura@users.noreply.github.com>
2021-09-12 19:58:07 +0900

    追加：悪夢

--- js/rmmz_core.js
+++ js/rmmz_core.js
@@ -1,5 +1,5 @@
 //=============================================================================
-// rmmz_core.js v1.3.0
+// rmmz_core.js v1.3.2
 //=============================================================================
 
 //-----------------------------------------------------------------------------
@@ -192,7 +192,7 @@ Utils.RPGMAKER_NAME = "MZ";
  * @type string
  * @constant
  */
-Utils.RPGMAKER_VERSION = "1.3.0";
+Utils.RPGMAKER_VERSION = "1.3.2";
 
 /**
  * Checks whether the current RPG Maker version is greater than or equal to
@@ -369,6 +369,16 @@ Utils.canPlayWebm = function() {
  */
 Utils.encodeURI = function(str) {
     return encodeURIComponent(str).replace(/%2F/g, "/");
+};
+
+/**
+ * Gets the filename that does not include subfolders.
+ *
+ * @param {string} filename - The filename with subfolders.
+ * @returns {string} The filename without subfolders.
+ */
+Utils.extractFileName = function(filename) {
+    return filename.split("/").pop();
 };
 
 /**
--- js/rmmz_objects.js
+++ js/rmmz_objects.js
@@ -1,5 +1,5 @@
 //=============================================================================
-// rmmz_objects.js v1.3.0
+// rmmz_objects.js v1.3.2
 //=============================================================================
 
 //-----------------------------------------------------------------------------
@@ -11280,7 +11280,8 @@ Game_Interpreter.prototype.pluginCommand = function() {
 
 // Plugin Command
 Game_Interpreter.prototype.command357 = function(params) {
-    PluginManager.callCommand(this, params[0], params[1], params[3]);
+    const pluginName = Utils.extractFileName(params[0]);
+    PluginManager.callCommand(this, pluginName, params[1], params[3]);
     return true;
 };
 
